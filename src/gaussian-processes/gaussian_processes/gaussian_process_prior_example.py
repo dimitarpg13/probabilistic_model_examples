01f42462a26214027d77d2d9ce22e5be082b79f3
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-04-13 21:11:42 -0400

    add project package updates

--- src/gaussian-processes/gaussian_processes/gaussian_process_prior_example.py
+++ src/gaussian-processes/gaussian_processes/gaussian_process_prior_example.py
@@ -1,2 +1,10 @@
 from __future__ import division
 import numpy as np
+import matplotlib.pyplot as pl
+
+def kernel(a, b):
+    """Gaussian Process squared exponential kernel"""
+    sqdist = np.sum(a**2,1).reshape(-1,1) + np.sum(b**2,1) - 2*np.dot(a, b.T)
+    return np.exp(-0.5*sqdist)
+
+
